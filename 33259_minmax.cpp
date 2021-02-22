ddff1d503a330864ddc3ffd1454574cc13c3a2d4
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-02-22 12:15:25 +0530

    minmax done

--- 33259_minmax.cpp
+++ 33259_minmax.cpp
@@ -1,64 +1,86 @@
-// Problem Statement - Find and display minimum and maximum numbers from an array
-
-// Pseudo Code
-// -------------------------------------------------------------------------------------
-// int min = array[0], max = array[0]   (1)
-// for i = 0 to (size of array):        (n+1)
-//     if(array[i] < min):              (n)
-//         min = array[i]               (n)
-//     if(array[i] > max):              (n)
-//         max = array[i]               (n)
-// print(min, max)                      (1)
-// -------------------------------------------------------------------------------------
-
-// Complexity Analysis - Time complexity = O(n)
-// -------------------------------------------------------------------------------------
-// f = 1 + n + 1 + n + n + n + n + 1
-// f = 5n + 3
-// -------------------------------------------------------------------------------------
-
-// Implementation
-// -------------------------------------------------------------------------------------
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+bool check_sorted(int *, int);
+void min_max(int *, int &, int &, int, int);
 
 int main()
 {
-    int *array;
+    int *arr, n, min = INT32_MAX, max = INT32_MIN;
 
-    int n;
-
-    cout << "Enter size of array:- ";
+    // Number of elements
+    cout << "\nEnter the number of elements in the array:- ";
     cin >> n;
+    arr = new int[n];
 
-    array = new int[n];
-
-    cout << "Enter elements in the array:- \n";
+    // Input elements from the user
+    cout << "\nEnter the elements:- \n";
     for (int i = 0; i < n; i++)
     {
         cout << "Enter element " << i << ":- ";
-        cin >> array[i];
+        cin >> arr[i];
     }
 
-    int min = array[0], max = array[0];
-
-    cout << "\nArray is:- ";
-    for (int i = 0; i < n; i++)
+    // If already sorted then don't divide and conquer
+    if (check_sorted(arr, n))
     {
-        cout << array[i] << " ";
-
-        if (array[i] < min)
-            min = array[i];
-        if (array[i] > max)
-            max = array[i];
+        min = arr[0];
+        max = arr[n - 1];
+        cout << "\nFinal minimum in the Array:- " << min;
+        cout << "\nFinal maximum in the Array:- " << max;
+        cout << "\n";
+        return 0;
     }
-    cout << "\n\n";
 
-    cout << "Maximum element is:- " << max << endl;
-    cout << "Minimum element is:- " << min << endl;
+    // If not sorted then divide and conquer
+    min_max(arr, min, max, 0, n - 1);
+    cout << "\nFinal minimum in the Array:- " << min;
+    cout << "\nFinal maximum in the Array:- " << max;
+
+    cout << "\n";
 
     return 0;
 }
 
-// -------------------------------------------------------------------------------------
+bool check_sorted(int *arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i - 1] >= arr[i])
+            return false;
+    }
+    return true;
+}
+
+void min_max(int *arr, int &min, int &max, int start, int end)
+{
+    if (end - start <= 1)
+    {
+        if (arr[start] > max)
+            max = arr[start];
+        if (arr[end] > max)
+            max = arr[end];
+        if (arr[start] < min)
+            min = arr[start];
+        if (arr[end] < min)
+            min = arr[end];
+        cout << "\nArray is:- ";
+        for (int i = start; i <= end; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\nMinimum in the Array:- " << min;
+        cout << "\nMaximum in the Array:- " << max << endl;
+    }
+    else
+    {
+        int mid = (start + end) / 2;
+        cout << "\nArray is:- ";
+        for (int i = start; i <= end; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        min_max(arr, min, max, start, mid);
+        min_max(arr, min, max, mid + 1, end);
+    }
+}
