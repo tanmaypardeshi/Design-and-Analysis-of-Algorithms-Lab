2a5af533dc5e6859a177b221e2f1a1fa287d4349
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-03-08 22:13:55 +0530

    Min max

--- 33259_minmax.cpp
+++ 33259_minmax.cpp
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------
+Roll number: 33259
+Division: 10
+Batch: N 10
+Problem Statement: Find minimum and maximum element of an array using divide
+and conquer strategy.
+----------------------------------------------------------------------------*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -84,3 +91,34 @@ void min_max(int *arr, int &min, int &max, int start, int end)
         min_max(arr, min, max, mid + 1, end);
     }
 }
+
+/* Output - 
+
+Enter the number of elements in the array:- 5
+
+Enter the elements:- 
+Enter element 0:- 4
+Enter element 1:- 3
+Enter element 2:- 2
+Enter element 3:- 5
+Enter element 4:- 1
+
+Array is:- 4 3 2 5 1 
+Array is:- 4 3 2 
+Array is:- 4 3 
+Minimum in the Array:- 3
+Maximum in the Array:- 4
+
+Array is:- 2 
+Minimum in the Array:- 2
+Maximum in the Array:- 4
+
+Array is:- 5 1 
+Minimum in the Array:- 1
+Maximum in the Array:- 5
+
+Final minimum in the Array:- 1
+Final maximum in the Array:- 5
+tanmay@Predator:~/Code/DAA$ 
+
+*/
