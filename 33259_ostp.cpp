0675a094dba1c1bdc47e9acb70197f7b01fe94d2
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-05-06 22:28:36 +0530

    OSTP done

--- 33259_ostp.cpp
+++ 33259_ostp.cpp
@@ -8,29 +8,10 @@ Problem Statement: Optimal storage in tapes.
 #include <bits/stdc++.h>
 using namespace std;
 
-float calculate(vector<int> tape)
-{
-    if (tape.size() == 0)
-        return 0;
-    float rt = 0;
-    for (int y = 0; y < tape.size(); y++)
-        rt += tape[y];
-    return rt / (float)tape.size();
-}
+float calculate_art(vector<int>);
+float calculate_mrt(vector<int>);
+int volume(vector<int>);
 
-int volume(vector<int> v)
-{
-    int total = 0;
-    if (v.size() == 0)
-    {
-        return 0;
-    }
-    for (auto x : v)
-    {
-        total += x;
-    }
-    return total;
-}
 int main()
 {
     vector<vector<int>> tapes;
@@ -94,7 +75,8 @@ int main()
         buffer++;
         if (flag >= no_tapes)
         {
-            break;
+            cout << "\nCannot accommodate programs in the tapes!" << endl;
+            return 0;
         }
     }
 
@@ -110,12 +92,109 @@ int main()
         cout << endl;
     }
 
-    cout << "\nMean Retirval Time of Tapes:-";
+    cout << "\nAverage Retrieval Time of Tapes:-";
     cntr = 1;
+    float art;
     for (auto x : tapes)
     {
-        cout << "\nMRT of tape " << cntr + 1 << ":- " << calculate(x);
+        float temp = calculate_art(x);
+        art += temp;
+        cout << "\nAverage Retrieval time of tape " << cntr << ":- " << temp;
+        cntr++;
     }
+    cout << "\nTotal Average Retrieval Time of Tapes is:- " << art / no_tapes;
+    cout << endl;
 
+    cout << "\nMean Retrieval Time of Tapes:- ";
+    cntr = 1;
+    float mrt;
+    for (auto x : tapes)
+    {
+        float temp = calculate_mrt(x);
+        mrt += temp;
+        cout << "\nMean Retrieval time of tape " << cntr + 1 << ":- " << temp;
+        cntr++;
+    }
+    cout << "\nTotal Mean Retrieval Time of Tapes is:- " << mrt / no_tapes;
+    cout << endl;
     return 0;
 }
+
+float calculate_art(vector<int> tape)
+{
+    if (tape.size() == 0)
+        return 0;
+    float rt = 0;
+    for (int y = 0; y < tape.size(); y++)
+        rt += tape[y];
+    return rt / tape.size();
+}
+
+float calculate_mrt(vector<int> tape)
+{
+    if (tape.size() == 0)
+        return 0;
+    float rt = 0;
+    for (int y = 0; y < tape.size(); y++)
+        rt += rt + tape[y];
+    return rt / tape.size();
+}
+
+int volume(vector<int> v)
+{
+    int total = 0;
+    if (v.size() == 0)
+    {
+        return 0;
+    }
+    for (auto x : v)
+    {
+        total += x;
+    }
+    return total;
+}
+
+/* Output - 
+
+tanmay@Predator:~/Code/DAA$ ./a.out 
+Enter the number of tapes:- 3
+
+Enter the size of each tape:- 
+Enter size of tape 0:- 50
+Enter size of tape 1:- 50
+Enter size of tape 2:- 50
+
+Enter the number of programs:- 9
+
+Enter the programs:- 
+Enter the length of program 1:- 4
+Enter the length of program 2:- 5
+Enter the length of program 3:- 6
+Enter the length of program 4:- 3
+Enter the length of program 5:- 2
+Enter the length of program 6:- 4
+Enter the length of program 7:- 5
+Enter the length of program 8:- 6
+Enter the length of program 9:- 7
+
+Original array is:- 4   5       6       3       2       4       5       6       7
+Sorted array is:- 2     3       4       4       5       5       6       6       7
+
+Contents of the tape are:- 
+Tape number 1:- 2       4       6
+Tape number 2:- 3       5       6
+Tape number 3:- 4       5       7
+
+Average Retrieval Time of Tapes:-
+Average Retrieval time of tape 2:- 4
+Average Retrieval time of tape 2:- 4.66667
+Average Retrieval time of tape 2:- 5.33333
+Total Average Retrieval Time of Tapes is:- 4.66667
+
+Mean Retrieval Time of Tapes:- 
+Mean Retrieval time of tape 2:- 7.33333
+Mean Retrieval time of tape 2:- 9.33333
+Mean Retrieval time of tape 2:- 11
+Total Mean Retrieval Time of Tapes is:- 9.22222
+
+*/
